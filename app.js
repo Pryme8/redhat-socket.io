0a75256a0201212d59c7ff7fb7a7f922d86e97f8
Andrew Butt <Pryme8@gmail.com>
2018-07-24 10:52:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+var port = 8080;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
@@ -16,3 +16,5 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+console.log("STARTING SERVER!");
